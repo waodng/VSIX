67cab45569f9d2dd2030b076d79f119e575a5b1e
waodng <454802367@qq.com>
2020-07-14 17:23:28 +0800

    dbchm window 布局和按钮添加

--- codemaid-10.8/CodeMaid/UI/Dialogs/DBCHM/DBCHMViewModel.cs
+++ codemaid-10.8/CodeMaid/UI/Dialogs/DBCHM/DBCHMViewModel.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Waodng.CodeMaid.Helpers;
+using Waodng.CodeMaid.Properties;
+using Waodng.CodeMaid.UI;
 
 /* ==============================================================================
  * 创建日期：2020/7/14 11:13:57
@@ -13,6 +17,31 @@ namespace Waodng.CodeMaid.UI.Dialogs.DBCHM
 {
     public class DBCHMViewModel : Bindable
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        private DelegateCommand _connectCommand;
 
+        /// <summary>
+        /// 数据库连接按钮
+        /// </summary>
+        public DelegateCommand ConnectCommand => _connectCommand ?? (_connectCommand = new DelegateCommand(OnConnectCommandExecuted));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        private void OnConnectCommandExecuted(object parameter)
+        {
+            try
+            {
+                MessageBox.Show("数据库连接操作");
+            }
+            catch (Exception ex)
+            {
+                //OutputWindowHelper.ExceptionWriteLine("数据库操作", ex);
+                //MessageBox.Show(ex.Message, "数据库操作", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
